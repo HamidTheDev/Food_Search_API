c95e875e6dbf78e8ca86954828bd22a4ff5e1ed6
Hamid Chowdhury <100404131+HamidTheDev@users.noreply.github.com>
2022-05-26 20:48:01 +0600

    reupload files

---
+++ js/search.js
@@ -0,0 +1,47 @@
+function search()
+{
+  // spiner
+  document.getElementById("spinner").style.display = "block";
+
+  let input = document.getElementById("input").value;
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
+
+  // search result for
+  const searchfor = document.getElementById("searchfor");
+  searchfor.innerText = ` Search Results for: '${input}'`;
+
+  // cleaner
+  document.getElementById("card_area").innerHTML = "";
+
+  document.getElementById("input").value = "";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => display(data.meals));
+}
+
+function display(data)
+{
+  // showing card
+  const cardArea = document.getElementById("card_area");
+
+  data.forEach((meal) => {
+    const card = document.createElement("div");
+
+    card.innerHTML = ` 
+  <div class="col">
+    <div class="card border-danger border-2">
+      <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p style="height: 300px;" class="card-text overflow-auto">${meal.strInstructions}</p>
+      </div>
+    </div>
+  </div> 
+    
+     `;
+    cardArea.appendChild(card);
+  });
+  // spiner
+  document.getElementById("spinner").style.display = "none";
+}
